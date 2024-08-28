12615636e8b518baf0e092a529b8c66529dbcec5
Mayco Matus <maykoleo90@gmail.com>
2024-08-28 17:40:52 -0300

    js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function mostrarRespuesta() {
+    const condicion = true;
+
+    const mostrarMensaje = document.getElementById('container-mensaje');
+    const mensajeVacio = document.getElementById('container-mensaje-vacio');
+}
